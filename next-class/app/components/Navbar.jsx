f0d0af4010c338b80e910b17f362b20b50f7abc1
sejalkumari12-ui <sejalkumari5679@gmail.com>
2025-11-23 20:53:01 +0530

    routing in next js

---
+++ next-class/app/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='flex text-4xl mb-10 bg-orange-400 justify-between items-center px-6 py-3'>
+      <h3>Sejal</h3>
+      <div className='flex gap-4'>
+       <Link href='/'>Home</Link>
+       <Link href='/about'>About</Link>
+       <Link href='/contact'>Contact</Link>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
