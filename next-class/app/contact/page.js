bc5522e2a659525bbb96cbe8eb82f55f320a09bc
sejalkumari12-ui <sejalkumari5679@gmail.com>
2025-11-23 14:25:40 +0530

    next js Routing

--- next-class/app/contact/page.js
+++ next-class/app/contact/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 function page() {
   return (
     <div>
-      <h1 className='text-4xl'>hello this is my home page </h1>
+       <h1 className='text-4xl'>hello this is my conact page </h1>
     </div>
   )
 }
